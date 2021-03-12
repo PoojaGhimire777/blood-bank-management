61c7fbfb9ebbff7284ac3f3d4ef90d42c30c388f
poojaghimire <pooja.ghimire777@gmail.com>
2021-03-12 20:12:58 +0530

    First Commit

---
+++ backend/src/test/java/com/bloodbank/BloodBankManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloodbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
